0eee12bc074e5d3b787bd2be8441212826f8ac6b
luankefei <luankefei@gmail.com>
2015-05-14 17:31:17 +0800

    add grid and tooltip

---
+++ src/js/compoent/grid.js
@@ -0,0 +1,20 @@
+
+
+T.define('grid', [], function(data) {
+
+    var defaults = {
+
+    }
+
+    function grid() {
+
+        create: function() {}
+
+        bind: function() {}
+
+        unbind: function() {}
+    }
+
+    return grid
+})
+
---
+++ src/js/compoent/tooltip.js
@@ -0,0 +1,20 @@
+
+
+T.define('tooltip', [], function(data) {
+
+    var defaults = {
+
+    }
+
+    function tooltip() {
+
+        create: function() {}
+
+        bind: function() {}
+
+        unbind: function() {}
+    }
+
+    return tooltip
+})
+
