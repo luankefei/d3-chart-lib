2101b68f5e5c2ff66a50be71c05db66192202877
luankefei <luankefei@gmail.com>
2015-05-14 18:11:55 +0800

    add title

---
+++ src/js/compoent/title.js
@@ -0,0 +1,20 @@
+
+
+T.define('title', [], function(data) {
+
+    var defaults = {
+
+    }
+
+    function title() {
+
+        create: function() {}
+
+        bind: function() {}
+
+        unbind: function() {}
+    }
+
+    return title
+})
+
